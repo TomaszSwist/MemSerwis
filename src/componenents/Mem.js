f1e39e0079064833aeeef0b588b96bcba0f605fa
Tomasz Świst <kontakt@tomaszswist.pl>
2024-05-01 13:50:14 +0200

    voting buttons added + voting local logic added

--- src/componenents/Mem.js
+++ src/componenents/Mem.js
@@ -1,9 +1,10 @@
-import SouthIcon from '@mui/icons-material/South';
-import NorthIcon from '@mui/icons-material/North';
+import SouthIcon from '@mui/icons-material/South'
+import NorthIcon from '@mui/icons-material/North'
+import Button from '@mui/material/Button'
+import { useState } from 'react'
 
 export function Mem(props) {
-
-    const mems = props.mems
+	const [mems, setMems] = useState(props.mems)
 
 	return (
 		<section>
@@ -11,8 +12,24 @@ export function Mem(props) {
 				<div key={mem.id}>
 					<h2>{mem.title}</h2>
 					<img src={`assets/mems/${mem.img}`} alt={mem.title} />
-                    <p><span>{mem.upvotes}</span><NorthIcon />
-                    <span>{mem.downvotes}</span><SouthIcon /></p>
+					<div>
+						<Button
+							onClick={() => {
+                                const newValue = mems[mem.id].upvotes + 1
+								setMems(mems.map(obj => (obj.id === mem.id ? { ...obj, upvotes: newValue } : obj)))
+							}}
+							variant='outlined'
+							endIcon={<NorthIcon />}>
+							{mem.upvotes}
+						</Button>
+						<Button onClick={() => {
+                                const newValue = mems[mem.id].downvotes + 1
+								setMems(mems.map(obj => (obj.id === mem.id ? { ...obj, downvotes: newValue } : obj)))
+							}}
+                            variant='outlined' endIcon={<SouthIcon />}>
+							{mem.downvotes}
+						</Button>
+					</div>
 				</div>
 			))}
 		</section>
