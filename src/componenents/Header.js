3e49790f0a984462e97f127bb2d57a1377377b53
Tomasz Świst <kontakt@tomaszswist.pl>
2024-06-02 19:19:13 +0200

    Added new route that will allow user to add img

--- src/componenents/Header.js
+++ src/componenents/Header.js
@@ -14,6 +14,7 @@ export function Header() {
 					<NavLink to='/'>Wszytkie</NavLink>
 					<NavLink to='/regular'>Regular</NavLink>
 					<NavLink to='/hot'>Hot</NavLink>
+					<NavLink to='/add'>Dodaj</NavLink>
 				</ButtonGroup>
 			</div>
 		</>
